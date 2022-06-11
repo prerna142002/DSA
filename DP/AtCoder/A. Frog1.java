563232d1506d2dde16719a65512b3643f8e73f46
Prerna Verma <55102903+prerna142002@users.noreply.github.com>
2022-06-11 23:51:10 +0530

    Update A. Frog1.java

--- DP/AtCoder/A. Frog1.java
+++ DP/AtCoder/A. Frog1.java
@@ -1,43 +1,35 @@
 import java.util.*;
-
 public class Main {
-
-	static int[] dp; 
-	public static int steps(int index, int[] heights)
-	{
-		if(index >= heights.length-1) return 0;
-		if(dp[index] != -1) return dp[index];
-		int one=0, two=0;
-		
-		one = steps(index+1,heights) + Math.abs(heights[index]-heights[index+1]);
-		if(index+2 >=heights.length) {
-			return dp[index] = one;
-		}
-		two = steps(index+2,heights) + Math.abs(heights[index]-heights[index+2]);
-		
-		
-		dp[index] = Math.min(one, two);
-//		System.out.println("index "+index);
-//		for(int p: dp) System.out.print(p+" ");
-//		System.out.println();
-		return dp[index];
-	}
 	
-	public static void main(String[] args) {
-		
-//		Scanner sc = new
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[] a = new int[n];
-		for(int i=0;i<n;i++)
-		{
-			a[i] = sc.nextInt();
-		}
-		dp = new int[n+1];
-		Arrays.fill(dp, -1);
-		steps(0,a);
-		System.out.println(dp[0]);
+	static int[] memo;
+	public static int func(int[] h, int index)
+	{
+		if( index >= h.length ) return 0;
+		if( memo[index] != -1  ) return memo[index];
+		int ans = 0;
+		if( index+1 < h.length ) ans = Math.abs(h[index]-h[index+1])+func(h,index+1);
+		if( index+2 < h.length ) ans = Math.min( ans , Math.abs(h[index]-h[index+2])+func(h,index+2));
+		memo[index] = ans;
+		return ans;
+	} 
+	
+    public static void main (String args[]) {
+       Scanner sc = new Scanner(System.in);
+       
+       if(sc.hasNext())
+       {
+//    	   int t = sc.nextInt();
+//    	   while(t-->0)
+//    	   {
+    		   int n = sc.nextInt();
+    		   int[] a = new int[n];
 
-	}
-
+    		   for(int i=0;i<n;i++) a[i] = sc.nextInt();
+    		   memo = new int[n];
+    		   Arrays.fill(memo, -1);
+    		   System.out.println(func(a,0));
+    		   
+//    	   }
+       }
+    }
 }
