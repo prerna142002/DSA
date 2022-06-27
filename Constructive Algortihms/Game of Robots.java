1ff4c835e9e1136eccc369c76bda40810c8a561f
Prerna Verma <55102903+prerna142002@users.noreply.github.com>
2022-06-27 11:13:25 +0530

    Update Game of Robots.java

--- Constructive Algortihms/Game of Robots.java
+++ Constructive Algortihms/Game of Robots.java
@@ -12,21 +12,31 @@ public class main {
     	   for(int i=0;i<n;i++) a[i] = sc.nextLong();
 
     	   long sum = 0, prev = 0;
-    	   for(int i=1;i<=n;i++)
+    	   int i =1;
+    	   for(;i<=n;i++)
     	   {
     		   sum+=i;
 //    		   System.out.println(sum);
-    		   if( sum > k ) break;
+    		   if( sum >= k ) break;
     		   prev = sum;
     	   }
-//    	   System.out.println(prev);
-    	   int index = -1;
-    	   while(prev < k )
+//    	   System.out.println("prev "+prev);
+    	   if( i== n && sum<k ) System.out.println("-1");
+    	   else 
     	   {
-    		   prev++;
-    		   index++;
+    		   int index = -1;
+        	   while(prev < k )
+        	   {
+        		   prev++;
+        		   index++;
+        	   }
+        	   if(index == -1 && prev == k) System.out.println(a[0]);
+        	   else System.out.println(a[index]);
+        		   
+        	   
     	   }
-    	   System.out.println(a[index]);
+//    	   System.out.println(prev);
+    	   
     	       	   
        }
     }
